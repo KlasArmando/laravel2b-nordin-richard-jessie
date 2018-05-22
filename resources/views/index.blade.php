bfbe29eae738c58a3d08923d97dfb4af34826458
nordin <nordinvanderleije@gmail.com>
2018-05-22 16:20:57 +0200

    change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Title</title>
+    <!--<link rel="stylesheet" href="css/materialize.css">-->
+    <!--<script src="js/materialize.js"></script>-->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>-->
 </head>
 <body>
-<h1>hai</h1>
+<a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
+
+<!-- Dropdown Structure -->
+<ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#">one</a></li>
+    <li><a href="#">two</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#">three</a></li>
+    <li><a href="#">four</a></li>
+    <li><a href="#">five</a></li>
+</ul>
+<script>
+
+    $(".dropdown-trigger").click(function () {
+        $(".dropdown-content").slideToggle();
+    });
+
+</script>
 </body>
 </html>
