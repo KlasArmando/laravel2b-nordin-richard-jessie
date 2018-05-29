b55c5a35c43db0232d25b644754d2e864c4dec77
nordin <nordinvanderleije@gmail.com>
2018-05-29 15:19:16 +0200

    made a login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -283,7 +283,6 @@
     @if (Route::has('login'))
         <div class="top-right links">
             @auth
-                <a href="{{ url('/home') }}">Home</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="{{ route('logout') }}"
                        onclick="event.preventDefault();
@@ -294,6 +293,10 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
                 </div>
             @else
                 <a href="{{ route('login') }}">Login</a>
