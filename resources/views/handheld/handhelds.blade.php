e6c4878302ee780b1a3fd86b508f7946d9299174
nordin <nordinvanderleije@gmail.com>
2018-06-19 13:02:02 +0200

    made some changes

---
+++ resources/views/handheld/handhelds.blade.php
@@ -0,0 +1,294 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+
+        * {box-sizing: border-box;}
+        body {
+            margin: 0;
+            font-family: Arial;
+        }
+
+        .header {
+            overflow: hidden;
+            background-color: #f45c42;
+            padding: 20px 10px;
+        }
+        .header a {
+            float: left;
+            color: black;
+            text-align: center;
+            padding: 12px;
+            text-decoration: none;
+            font-size: 18px;
+            line-height: 25px;
+            border-radius: 4px;
+        }
+        .header a.logo {
+            font-size: 25px;
+            font-weight: bold;
+        }
+        .header a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        .header a.active {
+            background-color: dodgerblue;
+            color: white;
+        }
+        .header-right {
+            float: Left;
+        }
+        @media screen and (max-width: 500px) {
+            .header a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+            .header-right {
+                float: none;
+            }
+        }
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #f45c42;
+            color: black;
+            text-align: Left;
+            text-decoration: none;
+            font-weight: normal;
+        }
+        a,
+        a:link,
+        a:visited,
+        a:active{
+            color: black;
+            text-align: Left;
+            text-decoration: none;
+            font-weight: normal;
+        }
+
+        #myInput {
+            background-image: url('{{asset('image/search.png')}}'); /* Add a search icon to input */
+            background-position: 10px 12px; /* Position the search icon */
+            background-repeat: no-repeat; /* Do not repeat the icon image */
+            width: 100%; /* Full-width */
+            font-size: 16px; /* Increase font-size */
+            padding: 12px 20px 12px 40px; /* Add some padding */
+            border: 1px solid #ddd; /* Add a grey border */
+            margin-bottom: 12px; /* Add some space below the input */
+        }
+
+        #myTable {
+            border-collapse: collapse; /* Collapse borders */
+            width: 100%; /* Full-width */
+            border: 1px solid #ddd; /* Add a grey border */
+            font-size: 18px; /* Increase font-size */
+        }
+
+        #myTable th, #myTable td {
+            text-align: left; /* Left-align text */
+            padding: 12px; /* Add padding */
+        }
+
+        #myTable tr {
+            /* Add a bottom border to all table rows */
+            border-bottom: 1px solid #ddd;
+        }
+
+        #myTable tr.header, #myTable tr:hover {
+            /* Add a grey background color to the table header and on hover */
+            background-color: #f1f1f1;
+        }
+    </style>
+</head>
+<body>
+<div class="header">
+    <a href="{{url('index')}}" class="logo">The Game Museum</a>
+    <div class="header-right">
+        <a href="{{url('index')}}">Home</a>
+        <a href="{{url('console')}}">Consoles</a>
+        <a href="{{url('games')}}">Games</a>
+        <a  class="active" href="{{url('handhelds')}}">Handheld</a>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                        </div>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+<br>
+@if(Auth::user()['role_id'] == 2)
+    <form action="{{url('handhelds/create')}}" method="GET">
+        <input type="submit" value="create">
+    </form>
+@endif
+<br>
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Handhelds..">
+<table id="myTable">
+    <tr class="header">
+        <th>Game</th>
+        <th>release date</th>
+        <th>company</th>
+        <th>price</th>
+    </tr>
+    @foreach($handhelds as $handheld)
+        <tr>
+            <td>
+                {{$handheld->naam}}
+            </td>
+            <td>
+                {{$handheld->releasedate}}
+            </td>
+            <td>
+                {{$handheld->company}}
+            </td>
+            <td>
+                $ {{$handheld->price}}
+            </td>
+            @if(Auth::user()['role_id'] == 2)
+                <td>
+                    <form action="{{url('handhelds/edit', $handheld->id)}}" method="GET">
+                        <input type="submit" value="edit">
+                    </form>
+                </td>
+                <td>
+                    <form action="{{url('/handhelds/' . $handheld->id)}}" method="POST">
+                        {{ csrf_field() }}
+                        @method('DELETE')
+                        <input type="submit" value="delete">
+                    </form>
+                </td>
+            @endif
+            <td>
+                <!-- ADD TO CART button code. -->
+                <form action="https://www.e-junkie.com/ecom/fgb.php?c=cart&cl=1&ejc=2" target="ej_ejc" method="POST">
+
+                    <!-- paypal email(remove if not using PayPal) -->
+                    <input type="hidden" name="business" value="your_paypal_email"/>
+
+                    <!-- site url -->
+                    <input type="hidden" name="site_url" value="http://yoursite.com"/>
+
+                    <!-- contact email (where we can notify of the updates) -->
+                    <input type="hidden" name="contact_email" value="your@email.address"/>
+
+                    <!-- item name -->
+                    <input type="hidden" name="item_name" value="{{$handheld->naam}}"/>
+
+                    <!-- item number (should be different for each product)-->
+                    <input type="hidden" name="item_number" value="{{$handheld->id}}"/>
+
+                    <!-- item price -->
+                    <input type="hidden" name="amount" value="{{$handheld->price}}"/>
+
+                    <!-- initial quantity -->
+                    <input type="hidden" name="quantity" value="1"/>
+
+                    <!-- shipping cost -->
+                    <input type="hidden" name="shipping" value="1">
+
+                    <!-- shipping cost of each additional unit -->
+                    <input type="hidden" name="shipping2" value="0.5">
+
+                    <!--handling cost -->
+                    <input type="hidden" name="handling" value="0.5">
+
+                    <!-- tax (flat amount, NOT percentage)-->
+                    <input type="hidden" name="tax" value="0.50"/>
+
+                    <!-- following options are applicable to whole cart-->
+
+                    <!-- you thank you page -->
+                    <input type="hidden" name="return_url" value="http://www.e-junkie.com/"/>
+
+                    <!-- any custom info you want to pass for the whole order -->
+                    <input type="hidden" name="custom" value="anything"/>
+
+                    <!-- currency (For PayPal: any currency that PayPal supports -->
+                    <input type="hidden" name="currency_code" value="USD"/>
+
+                    <input type="image" src="https://www.e-junkie.com/ej/ej_add_to_cart.gif" border="0" onClick="javascript:return EJEJC_lc(this.parentNode);">
+                </form>
+            </td>
+        </tr>
+        @endforeach
+</table>
+<div class="footer">
+    <a href="{{url('contact')}}">Contact</a>
+</div>
+
+<script language="javascript" type="text/javascript">
+    <!--
+    function EJEJC_lc(th) { return false; }
+    // -->
+</script>
+<script type="text/javascript"
+        src="https://www.e-junkie.com/ecom/box.js"></script>
+
+<script>
+    function myFunction() {
+        // Declare variables
+        var input, filter, table, tr, td, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+</script>
+</body>
+</html>
